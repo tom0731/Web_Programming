d333d7d1b3f07b00cc90f353b6523eed467a9b2b
Tommy156 <rock15688@gmail.com>
2016-09-28 06:05:22 +0800

    hw1_v2

--- Week1/todoList.js
+++ Week1/todoList.js
@@ -14,14 +14,23 @@ todoIn.onkeypress = function(e) {
 			newTodo.innerHTML = todoIn.value;
 			newTodo.done = false;
 			newTodo.onclick = function (e) {
-				if (!e.target.done) {
+				if (e.target.className === 'todo') {
 					e.target.innerHTML = e.target.innerHTML + " is done!";
 					stateChange(state[1], -1);
 					stateChange(state[2], 1);
 					e.target.done = true;
+					e.target.setAttribute('class', 'done');
+				}
+				else {
+					e.target.innerHTML = e.target.innerHTML.substring(
+						0, e.target.innerHTML.length - 8);
+					stateChange(state[1], 1);
+					stateChange(state[2], -1);
+					e.target.setAttribute('class', 'todo');
 				}
 			}
-			document.body.appendChild(newTodo);
+			document.getElementById("container").appendChild(newTodo);
+			// document.body.appendChild(newTodo);
 			for (var i in state) {
 				if (i < 2) {
 					stateChange(state[i], 1);
@@ -57,12 +66,19 @@ function removeTodo(rBtnId) {
 	var todolist; // object
 	var nRemove, todolistSize, removeIdx; // number
 	todolist = document.getElementsByClassName('todo');
+	donelist = document.getElementsByClassName('done');
 	nRemove = 0;
 	// todolistSize = todolist.length;
-	for (let i = 0; i < todolist.length; ++i) {
-		if ((rBtnId === 'Done' && todolist[i].done === true) ||
-			rBtnId === 'All') {
-			document.body.removeChild(todolist[i]);
+	for (let i = 0; i < donelist.length; ++i) {
+		document.getElementById("container").removeChild(donelist[i]);
+		// document.body.removeChild(todolist[i]);
+		nRemove += 1;
+		i -= 1;
+	}
+	if (rBtnId === 'All') {
+		for (let i = 0; i < todolist.length; ++i) {
+			document.getElementById("container").removeChild(todolist[i]);
+			// document.body.removeChild(todolist[i]);
 			nRemove += 1;
 			i -= 1;
 		}
