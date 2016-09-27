dbb1202a61c6ddd4188a3d8f042d6319af94faff
Tommy <r05943094@ntu.edu.tw>
2016-09-28 06:36:37 +0800

    add .gitignore and delete files

--- Week1/class1.js
+++
@@ -1,9 +0,0 @@
-console.log('fuck you');
-var bb = document.getElementById('btn');
-bb.addEventListener('click', function() {
-	// alert('fuck you : )')
-	var newFuck = document.createElement('div');
-	var newFuckText = document.createTextNode('FUCK YOUUU...');
-	newFuck.appendChild(newFuckText);
-	document.body.appendChild(newFuck);
-});
